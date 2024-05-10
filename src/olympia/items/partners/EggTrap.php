9532806635da85120b7a73fbf3617793ba936533
Nassim E <nassim.essid91@gmail.com>
2024-05-11 00:26:01 +0200

    fix some things and add eggtrap system

--- src/olympia/items/partners/EggTrap.php
+++ src/olympia/items/partners/EggTrap.php
@@ -5,8 +5,13 @@ namespace olympia\items\partners;
 use customiesdevs\customies\item\CreativeInventoryInfo;
 use customiesdevs\customies\item\ItemComponents;
 use customiesdevs\customies\item\ItemComponentsTrait;
+use pocketmine\block\VanillaBlocks;
+use pocketmine\entity\Entity;
 use pocketmine\item\Egg;
 use pocketmine\item\ItemIdentifier;
+use pocketmine\player\Player;
+use pocketmine\world\sound\BellRingSound;
+use pocketmine\math\Vector3;
 
 class EggTrap extends Egg implements ItemComponents {
     use ItemComponentsTrait;
@@ -18,6 +23,34 @@ class EggTrap extends Egg implements ItemComponents {
         $this->setLore([
             "§rCet objet permet de faire apparaître des §etoiles§r.",
         ]);
+    }
+
+    // temporary system
+    public function onAttackEntity(Entity $victim, array &$returnedItems): bool
+    {
+        if($victim instanceof Player) {
+            $world = $victim->getWorld();
+            $position = $victim->getPosition();
+            $positions = [
+                [$position->x, $position->y, $position->z],
+                [$position->x + 1, $position->y, $position->z],
+                [$position->x - 1, $position->y, $position->z],
+                [$position->x, $position->y, $position->z + 1],
+                [$position->x, $position->y, $position->z - 1],
+                [$position->x + 1, $position->y, $position->z + 1],
+                [$position->x - 1, $position->y, $position->z - 1],
+                [$position->x + 1, $position->y, $position->z - 1],
+                [$position->x - 1, $position->y, $position->z + 1],
+            ];
+
+            foreach ($positions as $pos) {
+                $world->setBlock(new Vector3($pos[0], $pos[1], $pos[2]), VanillaBlocks::COBWEB());
+            }
+
+            $victim->broadcastSound(new BellRingSound());
+            $victim->sendPopup("§cVous avez été frappé par un §eEggTrap§c.");
+        }
+        return true;
     }
 
 }
