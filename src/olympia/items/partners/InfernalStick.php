78ea300b37d6aae7b26d923488da3a03bc284ef0
Nassim E <nassim.essid91@gmail.com>
2024-05-11 00:21:24 +0200

    add InfernalStick system

--- src/olympia/items/partners/InfernalStick.php
+++ src/olympia/items/partners/InfernalStick.php
@@ -5,8 +5,16 @@ namespace olympia\items\partners;
 use customiesdevs\customies\item\CreativeInventoryInfo;
 use customiesdevs\customies\item\ItemComponents;
 use customiesdevs\customies\item\ItemComponentsTrait;
+use pocketmine\entity\effect\Effect;
+use pocketmine\entity\effect\EffectInstance;
+use pocketmine\entity\effect\EffectManager;
+use pocketmine\entity\effect\SpeedEffect;
+use pocketmine\entity\effect\VanillaEffects;
+use pocketmine\entity\Entity;
 use pocketmine\item\Item;
 use pocketmine\item\ItemIdentifier;
+use pocketmine\player\Player;
+use pocketmine\world\sound\BellRingSound;
 
 class InfernalStick extends Item implements ItemComponents {
     use ItemComponentsTrait;
@@ -18,5 +26,16 @@ class InfernalStick extends Item implements ItemComponents {
         $this->setLore([
             "§rCet objet permet de donné des §eeffets§r au joueur frappé.",
         ]);
+    }
+
+    public function onAttackEntity(Entity $victim, array &$returnedItems): bool
+    {
+        if($victim instanceof Player) {
+            $victim->getEffects()->add(new EffectInstance(VanillaEffects::BLINDNESS(), 25 * 5, 5));
+            $victim->getEffects()->add(new EffectInstance(VanillaEffects::SLOWNESS(), 25 * 5, 1));
+            $victim->broadcastSound(new BellRingSound());
+            $victim->sendPopup("§cVous avez été frappé par un §eBâton Infernal§c.");
+        }
+        return true;
     }
 }
