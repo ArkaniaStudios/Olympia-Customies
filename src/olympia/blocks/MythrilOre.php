3bfe0e6003f6f348f504a7b026dd473dd4bc0169
Nassim E <nassim.essid91@gmail.com>
2024-05-26 19:56:10 +0200

    add loot of ore 😒

--- src/olympia/blocks/MythrilOre.php
+++ src/olympia/blocks/MythrilOre.php
@@ -2,15 +2,26 @@
 
 namespace olympia\blocks;
 
+use olympia\items\OlympiaItems;
 use pocketmine\block\Block;
 use pocketmine\block\BlockBreakInfo;
 use pocketmine\block\BlockIdentifier;
 use pocketmine\block\BlockTypeIds;
 use pocketmine\block\BlockTypeInfo;
+use pocketmine\item\Item;
+use pocketmine\player\Player;
+use pocketmine\math\Vector3;
 
 class MythrilOre extends Block {
 
     public function __construct() {
         parent::__construct(new BlockIdentifier(BlockTypeIds::newId()), "Minerai de mythril", new BlockTypeInfo(new BlockBreakInfo(1)));
+    }
+
+    public function onBreak(Item $item, ?Player $player = null, array &$returnedItems = []): bool
+    {
+        $player?->getInventory()->addItem(OlympiaItems::MYTHRIL_INGOT());
+        $player?->getWorld()->dropExperience($player->getPosition(), 5);
+        return parent::onBreak($item, $player, $returnedItems);
     }
 }
--- src/olympia/blocks/OrichalqueOre.php
+++ src/olympia/blocks/OrichalqueOre.php
@@ -2,15 +2,26 @@
 
 namespace olympia\blocks;
 
+use olympia\items\OlympiaItems;
 use pocketmine\block\Block;
 use pocketmine\block\BlockBreakInfo;
 use pocketmine\block\BlockIdentifier;
 use pocketmine\block\BlockTypeIds;
 use pocketmine\block\BlockTypeInfo;
+use pocketmine\item\Item;
+use pocketmine\math\Vector3;
+use pocketmine\player\Player;
 
 class OrichalqueOre extends Block {
 
     public function __construct() {
         parent::__construct(new BlockIdentifier(BlockTypeIds::newId()), "Minerai d'orichalque", new BlockTypeInfo(new BlockBreakInfo(1)));
+    }
+
+    public function onBreak(Item $item, ?Player $player = null, array &$returnedItems = []): bool
+    {
+        $player?->getInventory()->addItem(OlympiaItems::ORICHALQUE_INGOT());
+        $player?->getWorld()->dropExperience($player->getPosition(), 10);
+        return parent::onBreak($item, $player, $returnedItems);
     }
 }
